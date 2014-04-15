d509db4090ec2d930595fc9b1765ef961c299919
jreuter <jreuter@wesleyan.edu>
2014-04-15 02:43:00 -0400

    'trippy' returned too many songs. change to 'weird animation'

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,7 +11,7 @@ class StaticPagesController < ApplicationController
   	@youtube_dev_key = "AI39si6_Eb75Wn08iBW2tQbPhzYEKuFuu1tBu_wF2QdcitIJz7rRVeZu4Z-MqvuRyyfjgaqXOWsSuT7LgWcLVaEh1oOS1vg1RA"
   	client = YouTubeIt::OAuth2Client.new(dev_key: @youtube_dev_key)
   	audio_videos = client.videos_by(:query => 'flying lotus', :page => 3, :per_page => 50).videos 
-  	visual_videos = client.videos_by(:query => 'trippy', :page => 1, :per_page => 50).videos
+  	visual_videos = client.videos_by(:query => 'weird animation', :page => 1, :per_page => 50).videos
  	n = rand(50)
  	m = rand(50)
  	@audio = audio_videos[n]
