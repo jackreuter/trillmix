da091bcf73b342a0265603267b8f1d4722905701
jreuter <jreuter@wesleyan.edu>
2014-04-15 14:07:56 -0400

    add inputs to the homepage to choose type of audio video (gif input does nothing yet)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def theatre
   	@youtube_dev_key = "AI39si6_Eb75Wn08iBW2tQbPhzYEKuFuu1tBu_wF2QdcitIJz7rRVeZu4Z-MqvuRyyfjgaqXOWsSuT7LgWcLVaEh1oOS1vg1RA"
   	client = YouTubeIt::OAuth2Client.new(dev_key: @youtube_dev_key)
-  	videos = client.videos_by(:query => "gil scott heron remix", :page => 1, :per_page => 50).videos
+  	aud_query = params['gif_aud']
+    videos = client.videos_by(:query => aud_query, :page => 1, :per_page => 50).videos
   	n = rand(50)
   	@audio = videos[n]
     gon.aud_id = @audio.video_id[-11..-1]
@@ -10,14 +11,16 @@ class StaticPagesController < ApplicationController
   def video_theatre
   	@youtube_dev_key = "AI39si6_Eb75Wn08iBW2tQbPhzYEKuFuu1tBu_wF2QdcitIJz7rRVeZu4Z-MqvuRyyfjgaqXOWsSuT7LgWcLVaEh1oOS1vg1RA"
   	client = YouTubeIt::OAuth2Client.new(dev_key: @youtube_dev_key)
-  	audio_videos = client.videos_by(:query => 'flying lotus', :page => 3, :per_page => 50).videos 
-  	visual_videos = client.videos_by(:query => 'weird animation', :page => 1, :per_page => 50).videos
- 	n = rand(50)
- 	m = rand(50)
- 	@audio = audio_videos[n]
- 	@video = visual_videos[m]	
-  gon.aud_id = @audio.video_id[-11..-1]
-  gon.vid_id = @video.video_id[-11..-1]
+  	aud_query = params['vid_aud']
+    vid_query = params['vid']
+    audio_videos = client.videos_by(:query => aud_query, :page => 1, :per_page => 50).videos 
+  	visual_videos = client.videos_by(:query => vid_query, :page => 1, :per_page => 50).videos
+ 	  n = rand(50)
+ 	  m = rand(50)
+ 	  @audio = audio_videos[n]
+ 	  @video = visual_videos[m]	
+    gon.aud_id = @audio.video_id[-11..-1]
+    gon.vid_id = @video.video_id[-11..-1]
   end
   def home
   end
